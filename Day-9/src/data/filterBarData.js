c37eea95ae06f9919313f61bcabfdb80ffa9a668
priyaadharshiniii <146160207+priyaadharshiniii@users.noreply.github.com>
2023-09-27 14:33:19 +0530

    Add files via upload

---
+++ Day-9/src/data/filterBarData.js
@@ -0,0 +1,63 @@
+export const sortMenu = [
+    {
+        id: 1,
+        title: "Latest",
+    },
+    {
+        id: 2,
+        title: "Featured",
+    },
+    {
+        id: 3,
+        title: "Top Rated",
+    },
+    {
+        id: 4,
+        title: "Price(Lowest First)",
+    },
+    {
+        id: 5,
+        title: "Price(Highest First)"
+    },
+];
+
+export const brandsMenu = [
+    {
+        id: 1,
+        label: "Drools",
+        checked: false,
+    },
+    {
+        id: 2,
+        label: "Pedicare",
+        checked: false,
+    },
+    {
+        id: 3,
+        label: "foodie-paw",
+        checked: false,
+    },
+];
+
+export const categoryMenu = [
+    {
+        id: 1,
+        label: "Drools",
+        checked: false,
+    },
+    {
+        id: 2,
+        label: "Pedicare",
+        checked: false,
+    },
+    {
+        id: 3,
+        label: "Petmart",
+        checked: false,
+    },
+    {
+        id: 4,
+        label: "Paw-treat",
+        checked: false,
+    },
+];
