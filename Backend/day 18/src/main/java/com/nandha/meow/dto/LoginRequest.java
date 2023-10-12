f933ca69062c0c507946af41b8c35824929941c3
priyaadharshiniii <146160207+priyaadharshiniii@users.noreply.github.com>
2023-10-12 15:36:15 +0530

    Add files via upload

---
+++ Backend/day 18/src/main/java/com/nandha/meow/dto/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.nandha.meow.dto;
+
+
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class LoginRequest {
+	private String email;
+	private String password;
+}
